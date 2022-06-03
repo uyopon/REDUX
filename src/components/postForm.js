86b8531dfedd3596a46b984949c636f94fd9b943
boss <andryousha228@gmail.com>
2022-06-04 00:17:20 +0500

    submitHundler

--- src/components/postForm.js
+++ src/components/postForm.js
@@ -8,12 +8,21 @@ export default class PostForm extends React.Component {
         this.state = {} //Ключевое слово super используется для доступа и вызова функций родителя объекта.
 
     }
+    submitHundler = event =>{
+        event.preventDefault()
+    }
+
+
     render() {
         return (
 
-            <div>
-                <h1>PostForm</h1>
-            </div>
+            <form onSubmit= {this.submitHundler}>
+                <div className="form-group">
+                    <label htmlFor="exampleInputPassword 1">Password</label>
+                    <input type= 'password' className="form-control" id="exampleInputPassword1"></input>
+
+                </div>
+            </form>
 
 
 
