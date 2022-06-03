d6a32b928497bcba555c40b1f8ec8d204faa9b38
boss <andryousha228@gmail.com>
2022-06-03 23:56:58 +0500

    button

--- src/components/fetchedPosts.js
+++ src/components/fetchedPosts.js
@@ -1,10 +1,9 @@
 import React from "react";
-export default ()=>{
-    return(
-        <div>
-            <h1>
-                Fetched Post
-            </h1>
-        </div>
-    )
+import Post from "./post";
+
+export default ({posts}) => {
+    if (!posts.length) {
+        return <button className= "btn btn-primary">загрузить</button>
+    }
+    return posts.map(post => <Post post={post} key={post} />) // ТЕГИ КОМПОНЕНТ ALWAYS WITH ЗАГЛАВНОЙ БУКВЫ
 }
